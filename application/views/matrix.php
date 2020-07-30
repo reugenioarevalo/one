d8c1bff716c5a066b970b79a46e5d38c86063083
Eugenio Arevalo <reugenioarevalo@gmail.com>
2020-07-30 16:28:44 -0300

    Initial commit

---
+++ application/views/matrix.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>matrix</title>
+  
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<canvas id=canvas>
+<!-- partial -->
+  <script  src="<?php echo base_url('assets/script.js');?>"></script>
+
+</body>
+</html>
