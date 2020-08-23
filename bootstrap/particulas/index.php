180ab6dff0f27fb88a9f2e341af041e04366e5f6
Eugenio <reugenioarevalo@gmail.com>
2020-08-22 23:30:12 -0300

    AHORA SI

--- bootstrap/particulas/index.php
+++ bootstrap/particulas/index.php
@@ -7,23 +7,122 @@
     <link rel="stylesheet" href="assets/css/bootstrap.css">
     <link rel="stylesheet" href="assets/css/bootstrap-grid.css">
     <link rel="stylesheet" href="assets/css/estilos.css">
-    <style>
-        /* body{
-            background-color: ddd;
-        }
-        #particles-js{
-            width: 100%;
-            height: 400px;
-        } */
-    </style>
+
 </head>
+<style>
+body {
+    background-color: #000;
+    color: #fff;
+    font-size: 18px;
+    font-family: 'Lato', sans-serif;
+    font-weight: 300;
+}
+</style>
 
 <body>
-
     <div id="particles-js"></div>
-    <header class="contenedor header">
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand " href="#">
+            <img src="assets/img/logoapaisado.jpg" width="150px" height="50px" />
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
+            aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Nosotros</a>
+                </li>
+                                                  <li class="nav-item"><a class="nav-link" href="#">Portafolios</a></li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contacto</a>
+                    </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Whatsapp</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">Redes</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdown01">
+                        <a class="dropdown-item" href="#">Twitter</a>
+                        <a class="dropdown-item" href="#">Instagram</a>
+                        <a class="dropdown-item" href="#">Linkedin</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="contenedor" >
+        <div class="contenedor jumbotron">
+            <div class="container">
+                <div class="">
+                    <div>
+                        <h1 class="display-4 mt-2 ">
+                            <span>TECNOLOGIAS</span>
+                        </h1>
+                        <h2 class="display-2 ">
+                            <span>INFORMATICAS</span>
+                        </h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <canvas id="canvas"></canvas>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <h2 class="text-success ">Creación</h2>
+                    <p>Participamos como socios creativos desde el análisis, desarrollo, construcción e
+                        implementación de un software que se adapta a tus necesidades.. </p>
+                    <p><a class="btn btn-dark" href="#" role="button">Contactanos &raquo;</a></p>
+                </div>
+                <div class="col-md-4">
+                    <h2 class="text-success">Programación</h2>
+                    <p>Programamos tu sistema, aplicación o página web,
+                        para hacer crecer digitalmente
+                        tu empresa y lo posicionamos
+                        difrenciandote de tus competidores. </p>
+                    <p>
+                        <a class="btn btn-dark text-white" href="#" role="button">
+                            Contactanos &raquo;
+                        </a>
+                    </p>
+                </div>
+                <div class="col-md-4">
+                    <h2 class="text-success">Heading</h2>
+                    <p> Nuestros desarrollos y aplicaciones son totalmente responsive.
+                        Esto quiere decir que tienen la capacidad de adptase
+                        a computadoras, tablets y moviles.
+                    </p>
+                    <p><a class="btn btn-dark  text-white" href="#" role="button">Contactanos &raquo;</a></p>
+                </div>
+            </div>
+
+            <hr>
+
+        </div> <!-- /container -->
+
+</div>
+
+    <footer class="container">
+        </p>
+
+        <span class="nav-link pull-right">2020 ©
+            <a href="http://eugenio.com.ar" class=" text-success">
+                Eugenio Arevalo
+            </a>
+        </span>
+        </p>
+    </footer>
+
+    <!-- <header class="contenedor header">
         
-        <!-- Barra de navegación -->
         <nav class="barra-navegacion">
             <ul>
                 <li>
@@ -40,7 +139,6 @@
         
         
         <canvas id="canvas"></canvas>
-        <!-- Título y descripción -->
         <div class="contenido-descripcion">
             <div class="container">
             <div class="card-deck">
@@ -66,8 +164,8 @@
         </div>
     </header>
         <footer>
-        </footer>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" ></script>
+        </footer> -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="assets/js/bootstrap.js"></script>
     <script src="assets/js/matrix.js"></script>
     <script src="assets/js/particles.js"></script>
