6f2331ca3d357031f1d96fda182b6518cb86e8cf
Eugenio <reugenioarevalo@gmail.com>
2020-08-22 21:15:53 -0300

    partiulas

---
+++ bootstrap/particulas/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>matrix</title>
+    <!-- <link rel="stylesheet" media="screen" href="assets/css/style.css"> -->
+    <link rel="stylesheet" href="assets/css/estilos.css">
+    <style>
+        /* body{
+            background-color: ddd;
+        }
+        #particles-js{
+            width: 100%;
+            height: 400px;
+        } */
+    </style>
+</head>
+
+<body>
+
+    <div id="particles-js"></div>
+    <header class="contenedor header">
+        
+        <!-- Barra de navegación -->
+        <nav class="barra-navegacion">
+            <ul>
+                <li>
+                    <a href="#" class="logo">
+                        <img src="assets/img/logoapaisado.jpg" width="150px" height="50px"/>
+                    </a>
+                </li>
+                <li><a href="#">Nosotros</a></li>
+                <li><a href="#">Portafolios</a></li>
+                <li><a href="#">Redes</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+        </nav>
+        
+        
+        <canvas id="canvas"></canvas>
+        <!-- Título y descripción -->
+        <div class="contenido-descripcion">
+            <div class="">
+                <h1 class="titulo">
+                    <span>Tecnologías</span>
+                    <span>Informáticas</span>
+                </h1>
+                <article class="descripcion">
+                <p>Ofremos servicios integrales y estrategias efectivas para cada cliente y modelo de negocio. 
+                </p>
+                <br>
+                <p>
+                    Participamos como socios creativos desde el análisis, desarrollo, construcción e 
+                    implementación de un software de tus necesidades.
+                </p>
+                <br>
+                <p>
+                    Programamos tu sitio web, para hacer crecer digitalmente tu empresa y lo posicionamos 
+                    difrenciandte de tu competencia. 
+                    Nuestras aplicaciones son adptables a computadoras, tablets y moviles </p>
+                    <a href="#" class="btn-link">unite</a>
+                </article>
+            </div>
+        </div>
+    </header>
+
+    <script src="assets/js/matrix.js"></script>
+    <script src="assets/js/particles.js"></script>
+    <script src="assets/js/app.js"></script>
+    <script src="../assets/js/particulas.js"></script>
+
+</body>
+
+</html>
