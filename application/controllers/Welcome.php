6fa9bed0dae8089624f67c1247b2592407c57ad8
Eugenio <reugenioarevalo@gmail.com>
2020-09-06 00:46:04 -0300

    curso con maxy max para los amigos

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,17 +3,42 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 	public function __construct() {
-        parent::__construct();
-           
+		parent::__construct();
+		
     }
 
 	public function index()
-	{
-		$this->load->view('matrix');
-	}
+	{	
+		if($this->ion_auth->logged_in()){
 
-	function album(){
-		$this->load->view('ejemplos/album/index');
+			$data['usuario']=$this->ion_auth->user()->row();
+
+			$idUsuario=$data['usuario'];
+			$data['imagen']=$this->db->query("select * from imagenes where id_user= ".$idUsuario->id." order by ruta ")->result_array();
+			$this->load->view('matrix', $data);
+
+		}else{
+			
+			redirect(base_url('auth/login'));
+		}
+
+	}
+	function mostraralbum($parametro){
+
+		if($this->is_login($parametro)==true){
+
+			$this->load->view('header');
+			$this->load->view('nav');
+			$this->load->view('ejemplos/album/index');
+			$this->load->view('footer');
+
+		}else{
+
+			$this->load->view('login');
+			redirect( base_url('welcome/album/true'));
+			
+		}
+		
 	}
 
 	function carusel(){
--- application/views/matrix.php
+++ application/views/matrix.php
@@ -1,26 +1,42 @@
 <!DOCTYPE html>
-<html lang="en" >
+<html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <title>matrix</title>
-  
+    <meta charset="UTF-8">
+    <title>Maxy</title>
+
 
 </head>
 <style>
-  #canvas {
-  width: 100%;
-  height: 100%;
+#canvas {
+    width: 100%;
+    height: 100%;
 }
 </style>
-<body>
-<!-- partial:index.partial.html -->
-<canvas id=canvas >
-<!-- partial -->
-<script>
-    var ancho = screen.width;
 
-  </script>
-  <script  src="<?php echo base_url('assets/script.js');?>"></script>
+<body>
+    <!-- partial:index.partial.html -->
+    <h1> Hola <?php echo $usuario->first_name; ?></h1>
+    <a href="<?php echo base_url('auth/logout');?>"> SALir</a>
+
+
+
+    <?php foreach($imagen as $fila){ ?>
+
+      <img src=" <?php echo base_url($fila['ruta']); ?>" />
+      <p> <?php echo base_url($fila['ruta']); ?>"</p>
+
+    <?php }?>
+
+
+
+    <canvas id=canvas>
+        <!-- partial -->
+        <script>
+        var ancho = screen.width;
+        </script>
+        <script src="<?php echo base_url('assets/js/matrix.js');?>"></script>
 
 </body>
+
 </html>
