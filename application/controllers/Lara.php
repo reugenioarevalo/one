0ab99610f4f8edd4213e5e64fe59c4c272daa92d
Eugenio <reugenioarevalo@gmail.com>
2020-09-26 00:35:09 -0300

    subo

--- application/controllers/Lara.php
+++ application/controllers/Lara.php
@@ -1,30 +1,44 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Lara extends CI_Controller {
-	public function __construct() {
+class Lara extends CI_Controller
+{
+    public function __construct()
+    {
         parent::__construct();
         $this->load->helper('html');
     }
 
-	public function index()
-	{
-        echo heading('BIENVENIDO a LARA gYm!', 1, 'style="text-align: center; background-color:green; color:white;"');
-        
-        echo anchor('lara/usuarios', 'Usuarios' ).'<br>';
-        echo anchor('lara/entrenamientos', 'Entrenamientos').'<br>';
-        echo anchor('lara/reportes', 'Reportes').'<br>';
+    public function index()
+    {
+        echo heading(
+            'BIENVENIDO a LARA gYm!',
+            1,
+            'style="text-align: center; background-color:green; color:white;"'
+        );
+
+        echo anchor('lara/usuarios', 'Usuarios') . '<br>';
+        echo anchor('lara/entrenamientos', 'Entrenamientos') . '<br>';
+        echo anchor('lara/reportes', 'Reportes') . '<br>';
     }
 
-    public function usuarios(){
-        echo heading('USUARIOS', 1, 'style="text-align: center; background-color:green; color:white;"');
-        
-        echo anchor('usuarios/getUsuarios', 'Listar todos los Usuarios' ).'<br>';
-        echo anchor('usuarios/getUsuarioId', 'Listar Usuario por ID').'<br>';
-        echo anchor('lara/', 'Volver').'<br>';
+    public function usuarios()
+    {
+        echo heading(
+            'USUARIOS',
+            1,
+            'style="text-align: center; background-color:green; color:white;"'
+        );
+
+        echo anchor('usuarios/getUsuarios', 'Listar todos los Usuarios') . '<br>';
+        echo anchor('usuarios/getUsuarioId', 'Listar Usuario por ID') . '<br>';
+        echo anchor('lara/', 'Volver') . '<br>';
     }
 
-    public function entrenamientos(){}
-    public function reportes(){}
-	
+    public function entrenamientos()
+    {
+    }
+    public function reportes()
+    {
+    }
 }
